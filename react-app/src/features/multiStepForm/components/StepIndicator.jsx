c2cf1ccaab4d7a533682eb97dd4026c5da9d98a3
João Sena <joaosena0206@gmail.com>
2025-03-20 18:28:00 -0300

    feat: mostra a etapa selecionada

--- react-app/src/features/multiStepForm/components/StepIndicator.jsx
+++ react-app/src/features/multiStepForm/components/StepIndicator.jsx
@@ -1,15 +1,27 @@
 import StepItemIndicator from "./StepItemIndicator";
 
+const STEPS_AMOUNT = 4;
+
 function StepIndicator({ selectedStep }) {
+	let steps = [];
+
+	for (let i = 0; i < STEPS_AMOUNT; i++) {
+		let stepNow = i + 1;
+
+		steps[i] = (
+			<li>
+				<StepItemIndicator
+					step={stepNow}
+					isSelected={selectedStep == stepNow}
+				/>
+			</li>
+		);
+	}
+
 	return (
 		<div
 			className={`bg-[url("src/features/multiStepForm/images/bg-sidebar-mobile.svg")] h-[172px]`}>
-			<ul className="flex justify-center space-x-5 p-5 py-10">
-				<StepItemIndicator step={1} />
-				<StepItemIndicator step={2} />
-				<StepItemIndicator step={3} />
-				<StepItemIndicator step={4} />
-			</ul>
+			<ul className="flex justify-center space-x-5 p-5 py-10 text-white">{steps}</ul>
 		</div>
 	);
 }
--- react-app/src/features/multiStepForm/components/StepItemIndicator.jsx
+++ react-app/src/features/multiStepForm/components/StepItemIndicator.jsx
@@ -1,6 +1,9 @@
-function StepItemIndicator({ step }) {
+function StepItemIndicator({ step, isSelected }) {
 	return (
-		<div className="rounded-full border-[1px] border-white text-white w-[35px] h-[35px] flex items-center justify-center font-bold">
+		<div
+			className={`rounded-full border-[1px] border-white w-[35px] h-[35px] flex items-center justify-center font-bold ${
+				isSelected && "bg-[#C0E2FD] text-black border-none"
+			}`}>
 			{step}
 		</div>
 	);
