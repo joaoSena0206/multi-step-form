585dc506dbcd9a93af3a50f6a9c5ac785ddfb9cf
João Sena <joaosena0206@gmail.com>
2025-03-22 11:07:01 -0300

    feat: checa tamanho do telefone

--- react-app/src/features/multiStepForm/utils/validatePersonalInfo.js
+++ react-app/src/features/multiStepForm/utils/validatePersonalInfo.js
@@ -8,6 +8,8 @@ export default function validatePersonalInfo(data) {
 		phone: "",
 	};
 
+	let phone = data.phone.replace(/[\s\-()]/g, "");
+
 	if (data.name == "") {
 		errors.invalid = true;
 		errors.name = "Name is required!";
@@ -16,15 +18,17 @@ export default function validatePersonalInfo(data) {
 	if (data.email == "") {
 		errors.invalid = true;
 		errors.email = "Email is required!";
-	}
-	else if (!validate(data.email)) {
+	} else if (!validate(data.email)) {
 		errors.invalid = true;
 		errors.email = "Invalid Email!";
 	}
 
-	if (data.phone == "") {
+	if (phone == "") {
 		errors.invalid = true;
 		errors.phone = "Phone number is required!";
+	} else if (phone.length < 11) {
+		errors.invalid = true;
+		errors.phone = "Phone number must be 11 digits!";
 	}
 
 	return errors;
