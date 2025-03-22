8ff2637b15ec2c6f87bf0ceb771f7980b85036b0
João Sena <joaosena0206@gmail.com>
2025-03-22 14:18:26 -0300

    fix: corrige palava errada

--- react-app/src/features/multiStepForm/data/data.js
+++ react-app/src/features/multiStepForm/data/data.js
@@ -35,7 +35,7 @@ export const steps = [
 	{
 		id: 2,
 		title: "Select your plan",
-		description: "You have the option of monthly or yearely billing.",
+		description: "You have the option of monthly or yearly billing.",
 	},
 	{
 		id: 3,
