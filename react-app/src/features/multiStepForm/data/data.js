12db5cd1f01a3188299aebfeaae466ba16cd7778
João Sena <joaosena0206@gmail.com>
2025-03-23 10:33:27 -0300

    chore: adiciona addons

--- react-app/src/features/multiStepForm/data/data.js
+++ react-app/src/features/multiStepForm/data/data.js
@@ -1,3 +1,33 @@
+export const addons = [
+	{
+		id: 1,
+		name: "Online service",
+		description: "Access to multiplayer games",
+		price: {
+			monthly: 1,
+			yearly: 10,
+		},
+	},
+	{
+		id: 2,
+		name: "Larger storage",
+		description: "Extra 1TB of cloud save",
+		price: {
+			monthly: 2,
+			yearly: 20,
+		},
+	},
+	{
+		id: 3,
+		name: "Customizable profile",
+		description: "Custom theme on your profile",
+		price: {
+			monthly: 2,
+			yearly: 20,
+		},
+	},
+];
+
 export const plans = [
 	{
 		id: 1,
