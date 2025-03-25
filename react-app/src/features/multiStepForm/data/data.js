6ae6c09cbfcdd6ae040f0cfbcff3acf949b0c441
João Sena <joaosena0206@gmail.com>
2025-03-25 17:01:14 -0300

    chore: adiciona titulo de indicador de etapa

--- react-app/src/features/multiStepForm/data/data.js
+++ react-app/src/features/multiStepForm/data/data.js
@@ -60,21 +60,25 @@ export const steps = [
 		id: 1,
 		title: "Personal info",
 		description:
-			"Please provide your name, email address, and phone numbe.",
+			"Please provide your name, email address, and phone number.",
+		stepIndicator: "YOUR INFO",
 	},
 	{
 		id: 2,
 		title: "Select your plan",
 		description: "You have the option of monthly or yearly billing.",
+		stepIndicator: "SELECT PLAN",
 	},
 	{
 		id: 3,
 		title: "Pick add-ons",
 		description: "Add-ons help enhance your gaming experience.",
+		stepIndicator: "ADD-ONS",
 	},
 	{
 		id: 4,
 		title: "Finishing up",
 		description: "Double-check everything looks OK before confirming.",
+		stepIndicator: "SUMMARY",
 	},
 ];
